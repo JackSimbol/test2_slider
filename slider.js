2def5e558c10d82e6f65d4de60262db868e6b848
Xingbo Wang <91768006+JackSimbol@users.noreply.github.com>
2022-08-21 08:46:46 +0800

    Update slider.js

--- slider.js
+++ slider.js
@@ -2,9 +2,13 @@ const sliderEl = document.querySelector("#slider-input")
 
 const selectedEl = document.querySelector(".selected")
 
-sliderEl.addEventListener("input", () => {
+sliderEl.addEventListener("change", () => { //根据滑块值修改填写值
     selectedEl.innerHTML = sliderEl.value;
 });
+
+selectedEl.addEventListener("input", () =>{ //根据输入修改滑块值
+    sliderEl.innerHTML = selectedEl.value;  
+})
 
 function send(){
     var aa = selectedEl;
